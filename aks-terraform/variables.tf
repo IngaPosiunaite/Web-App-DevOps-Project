9c04f42505a79a279f15d25ca55410cde726937e
Inga Posiunaite <ingeera@gmail.com>
2024-02-09 12:29:43 +0000

    Adds Security Credentials

--- aks-terraform/variables.tf
+++ aks-terraform/variables.tf
@@ -10,3 +10,16 @@ variable "client_secret" {
   sensitive   = true
 }
 
+variable "subscription_id" {
+  description = "Secret key for the provider"
+  type        = string
+  sensitive   = true
+}
+
+
+variable "tenant_id" {
+  description = "Secret key for the provider"
+  type        = string
+  sensitive   = true
+}
+
